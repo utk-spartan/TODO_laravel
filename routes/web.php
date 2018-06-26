58c2758d122f14c8541f01e9a132d0a40831fcf2
Nisheet Kumar <nisheetkumar@apples-MBP-2.localdomain>
2018-06-26 16:03:00 +0530

    Update todo list

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,23 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//namespace App\Http\Kernel;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::patch('/tasks/{id}/{task}/',function ($id,$task){
+    //for updating task file
+    //$input = $request->all();
+
+    DB::table('tasks')
+        ->where('id', $id)
+        ->update(['task' => $task]);
+    return response()->json([
+        'id' => $id,
+        'task'=>$task,
+        'state' => 'updated'
+    ]);
+    //return view('welcome');
 });
