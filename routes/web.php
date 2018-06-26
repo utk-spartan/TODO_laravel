9977be03afa3e3cc10b8d1a5e35876b175e3f7b9
Nisheet Kumar <nisheetkumar@apples-MBP-2.localdomain>
2018-06-26 17:49:33 +0530

    Add update todo list

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::patch('/tasks/id',function (\Illuminate\Http\Request $request){
+    //for updating task file
+    //$input = $request->all();
+    $id = $name = $request->input('id');
+    $task = $name = $request->input('task');
+    DB::table('tasks')
+        ->where('id', $id)
+        ->update(['task' => $task]);
+    return response()->json([
+        'id' => $id,
+        'task'=>$task,
+        'state' => 'updated'
+    ]);
+    //return view('welcome');
+});
