71c8ff2241a020e19b1954a3c640bc540abea336
Nisheet Kumar <nisheetkumar@apples-MBP-2.localdomain>
2018-06-27 13:39:19 +0530

    MVC pattern implementation

--- app/Http/Controllers/Todo.php
+++ app/Http/Controllers/Todo.php
@@ -18,4 +18,29 @@ class Todo extends Controller
         $deletedRows = Tasks::where('id', $id)->delete();
         return ['no of deleted rows'=> $deletedRows];
     }
+    public function update($id=null,$task=null)
+    {
+            //for updating task file
+            //$input = $request->all();
+            //$id = $name = $request->input('id');
+            //$task = $name = $request->input('task');
+        if($id===null)
+        {
+            echo "enter task id to update a task";
+        }
+        else if($task==null)
+        {
+            echo "enter task to update";
+        }
+        else {
+            Tasks::where('id', $id)
+                ->update(['task' => $task]);
+            return response()->json([
+                'id' => $id,
+                'task' => $task,
+                //'state' => 'updated'
+            ]);
+        }
+            //return view('welcome');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,25 +20,7 @@ Route::post('/tasks', function (\Illuminate\Http\Request $request) {
 
 });
 
-Route::patch('/tasks/id',function (\Illuminate\Http\Request $request){
-    //for updating task file
-    //$input = $request->all();
-    $id = $name = $request->input('id');
-    $task = $name = $request->input('task');
-    DB::table('tasks')
-        ->where('id', $id)
-        ->update(['task' => $task]);
-    return response()->json([
-        'id' => $id,
-        'task'=>$task,
-        'state' => 'updated'
-    ]);
-    //return view('welcome');
-});
-
-
-
-
+Route::patch('/tasks/{id?}/{task?}','Todo@update')->where('id','[0-9]+');
 
 
 Route::delete('tasks/{id}', 'Todo@delete')->where('id', '[0-9]+');
